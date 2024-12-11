474809b9fd633269847a01f13623e5054ade65c0
Kirill <kerya021003@gmail.com>
2024-12-11 10:48:43 +0300

    Add delete method

--- diary-app/src/main/java/kirill/app/diary/controllers/MainController.java
+++ diary-app/src/main/java/kirill/app/diary/controllers/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -107,6 +108,12 @@ public class MainController {
         noteService.updateNote(note); 
 
         return "redirect:/note/" + id;
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public String deleteNote(@PathVariable("id") Long id) {
+    	noteService.deleteNoteById(id);
+    	return "redirect:/home";
     }
     
 	@GetMapping("/login")
